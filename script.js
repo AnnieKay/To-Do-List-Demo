ebcd0d7c699c237110b0f2b8d9e4877c6c6fcf42
anneKay <roselineu@gmail.com>
2017-03-30 04:17:00 +0100

    javascript-file

---
+++ script.js
@@ -0,0 +1,5 @@
+//add interactivity to the todo list
+var inputTask= document.getElementById("new-task");
+var addButton= document.getElementById("add");
+var uncompletedTasks= document.getElementById("todolists");
+var completedTasks= document.getElementById("completed-tasks");
